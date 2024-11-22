5f5016e0e5c77a05b3a09c7dc5955d4ad749364a
AleGomez15 <aleg34341@gmail.com>
2024-11-22 17:14:19 -0600

    Create programa2.py

---
+++ código/programa2.py
@@ -0,0 +1,7 @@
+#Explicacion del programa2
+
+print("¡Hola! ¿Cómo estás?")
+Cuando lo ejecutes, te mostrará el mensaje:
+¡Hola! ¿Cómo estás?
+
+Este es un código básico que utiliza la función print() para mostrar un mensaje en la consola.
