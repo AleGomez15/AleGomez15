1644ec2afbab545bdb3abda9aea8184cb94f2fe6
AleGomez15 <aleg34341@gmail.com>
2024-11-22 17:29:16 -0600

    Update programa2.py

--- código/programa2.py
+++ código/programa2.py
@@ -2,6 +2,6 @@
 
 print("¡Hola! ¿Cómo estás?")
 Cuando lo ejecutes, te mostrará el mensaje:
-¡Hola! ¿Cómo estás?
+¡Hola! ¿Cómo estás?:)
 
 Este es un código básico que utiliza la función print() para mostrar un mensaje en la consola.
