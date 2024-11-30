936c12cec8db129e7ed56e7d901cda98aab238f9
Ale Gomez <aleg34341@gmail.com>
2024-11-30 14:57:03 -0600

    Create bloque2p4.py

---
+++ código/bloque2p4.py
@@ -0,0 +1,6 @@
+# P.4 Programa que calcule los impuestos de un valor.
+# 22/10/2024
+# ALEJANDRA GUADALUPE GOMEZ CRUZ
+salario = int(input("Ingrese su salario: "))
+impuestos = salario * 0.16
+print("los impuestos son:" + str(impuestos) + " pesos" + "\n ¡Gracias por usar nuestro sistema!")
