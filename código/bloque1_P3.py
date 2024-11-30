bf990e892c74044b3bd9740e3548b3457664ae32
Ale Gomez <aleg34341@gmail.com>
2024-11-30 13:27:09 -0600

    Update bloque1_P3.py

--- código/bloque1_P3.py
+++ código/bloque1_P3.py
@@ -1,9 +1,9 @@
 #programa 1.3 muestra el numero de dias de la semana, el nombre de una figura de tres lados, a cuantos metros equivale 250 cm
 # fecha: 5/10/2024
-# nombre: Alejandra Gomez Cruz
+# nombre: Alexandra Rodriguez Gomez
 #numero de dias de la semana
 
-print("Cuantos dÃ¬as tiene la semana ")
+print("Cuantos dias tiene la semana ")
 print("7")
 
 #nombre de la fugura de tres lados
