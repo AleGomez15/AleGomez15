ec9663a445ead06e1b209eaa7f7ecccf637f1694
Ale Gomez <aleg34341@gmail.com>
2024-11-30 13:31:02 -0600

    Create bloque1_P4.py

---
+++ código/bloque1_P4.py
@@ -0,0 +1,10 @@
+#programa 1.4  comprobar el funcionamiento de unir cadenas
+# fecha: 5/10/2024
+# nombre:Alejandra Guadalupe Gomez Cruz
+#esto funciona bien
+print(1 + 2)
+print(1 - 2)
+print(1 * 2)
+print("sumando cadenas " + "esta bien")
+#esto no funciona
+print("no es posible " - "restar cadenas")
