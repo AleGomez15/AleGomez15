a63cb6bdd8663b217cad517f2777c68be5f7a9dd
Ale Gomez <aleg34341@gmail.com>
2024-12-03 11:24:10 -0600

    Create P4.py

---
+++ código/bloque4/P4.py
@@ -0,0 +1,14 @@
+#Programa.4 Programa que imprima el nombre de mis 10 personajes favoritos
+# Fecha: 2024/11/04
+# Elaborado por: Alejandra Guadalupe Gomez Cruz
+
+
+personajes = ["ironman","spiderman","Loki","Max","Mike","Once",
+              "Bach","imFER","Rapunzel","Bart Simpson"]
+
+# Imprimir cada personaje en la lista
+print("Mis 10 personajes favoritos son:")
+for personajes in personajes:
+    print(personajes)
+
+print("Gracias por ver mis 10 personajes favoritos")
