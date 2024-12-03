cf7f3ef4cd227b233023a4a6a722ab24ec7de2c4
Ale Gomez <aleg34341@gmail.com>
2024-12-03 11:20:30 -0600

    Create P2.py

---
+++ código/bloque4/P2.py
@@ -0,0 +1,6 @@
+# Programa 2 programa que imprima los nombres 
+# Fecha: 2024/11/04
+# Elaborado por:Alejandra Guadalupe Gomez Cruz
+nombres = ["Luis", "Chuy", "Mauricio"]
+for nombre in nombres: 
+    print("El nombre es: ",nombre)
