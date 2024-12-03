6a6660ccc7b849c8cd99b7c6b32966536d99c58d
Ale Gomez <aleg34341@gmail.com>
2024-12-03 11:27:15 -0600

    Create P8.py

---
+++ código/bloque4/P8.py
@@ -0,0 +1,20 @@
+# Programa 8
+# Fecha: 2024/11/07
+# Elaborado por: Alejandra Guadalupe Gomez Cruz
+
+# Ejemplo 1 
+# Imprimir los numeros del 1 al 10 
+i = 1 #Inicializacion de la variable de control 
+while i <= 10: # Condicion de parada
+    print(i)
+    i += 1 # Equivalente a i = i + 1 
+# Sintaxis:
+# while <condicional>:
+# <cuerpo del while>
+
+# Ejemplo 2 
+# Imprimir los numeros del 10 al 1 
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1 
