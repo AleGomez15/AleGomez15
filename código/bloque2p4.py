f85385e68cc3a1b854c1d998b2e1d56aeb34490d
Ale Gomez <aleg34341@gmail.com>
2024-12-05 14:19:59 -0600

    Delete código/bloque2p4.py

--- código/bloque2p4.py
+++
@@ -1,6 +0,0 @@
-# P.4 Programa que calcule los impuestos de un valor.
-# 22/10/2024
-# ALEJANDRA GUADALUPE GOMEZ CRUZ
-salario = int(input("Ingrese su salario: "))
-impuestos = salario * 0.16
-print("los impuestos son:" + str(impuestos) + " pesos" + "\n ¡Gracias por usar nuestro sistema!")
