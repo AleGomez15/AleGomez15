52d5b11ef2df5de800d3b7ec314f690245ba5144
Ale Gomez <aleg34341@gmail.com>
2024-12-05 14:19:20 -0600

    Delete código/bloque2p3.py

--- código/bloque2p3.py
+++
@@ -1,5 +0,0 @@
-#p.3 Programa que comprueba el funcionamiento de la adicion o concatenacion de varias cadenas de texto (strings)
-# 22/10/24
-# Hecho por: Alejandra Guadalupe Gomez Cruz
-
-print("Mi " + "nombre " + "es " + "Alejandra")
