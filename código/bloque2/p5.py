afc5015110ac15a3dbfa3de10394a511bd53ad1e
Ale Gomez <aleg34341@gmail.com>
2024-12-05 14:21:22 -0600

    Create p5.py

---
+++ código/bloque2/p5.py
@@ -0,0 +1,23 @@
+# P.5 Comparacion , Membership , indexing , metodo de slicing
+# 23/10/2024
+# Alejandra Guadalupe Gomez Cruz
+# COMPARACION
+print("perro" == "perro") #true
+print("perro" != "gato") #true
+print("Aguascalientes" < "Zacatecas") #true
+print("Aire" >= "Agua") #true
+print("\n ")
+# MEMBERSHIP
+print("house" in "boathouse") #true
+print("bien" in "bienvenidos") #true
+print("y" not in "ejes") #true
+print("je" in "ejes") #true
+print("\n ")
+# INDEXING(indexado)
+mi_nombre = "Alexandra"
+print(mi_nombre [3])
+print(mi_nombre [2])
+print("\n ")
+# SUBSCRIPTING 
+nombre = "Alejandra Gomez"
+print(nombre [2:6])
