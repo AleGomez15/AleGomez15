829bf930450754f2a3beeb39ed997fb329512331
Ale Gomez <aleg34341@gmail.com>
2024-12-05 14:21:42 -0600

    Delete código/bloque2p6.py

--- código/bloque2p6.py
+++
@@ -1,8 +0,0 @@
-# P.6  negative subscript indices
-# 24/10/24 
-# Alejandra guadalupe gomez cruz
-word = "hamster"
-print(word [ -1 ] ) 
-print(word [ -1:1 ] ) 
-print(word [ -3: ] ) 
-print(word [ :3 ] ) 
